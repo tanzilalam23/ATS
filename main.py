fa530eb71cbe34c4bea738f7163d709a779f42da
Namranotavailable <47214246+tanzilalam23@users.noreply.github.com>
2024-02-09 16:56:47 +0100

    commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 load_dotenv()
+import base64
 import streamlit as st
 import os
-from PIL import Image
-import pdf2image
-import base64
 import io
+from PIL import Image 
+import pdf2image
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -51,7 +51,9 @@ if uploaded_file is not None:
 
 submit1 = st.button("Tell Me About the Resume")
 
-submit2 = st.button("Percentage match")
+#submit2 = st.button("How Can I Improvise my Skills")
+
+submit3 = st.button("Percentage match")
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -59,7 +61,7 @@ input_prompt1 = """
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
-input_prompt2 = """
+input_prompt3 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
@@ -74,10 +76,10 @@ if submit1:
     else:
         st.write("Please uplaod the resume")
 
-elif submit2:
+elif submit3:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        response=get_gemini_response(input_prompt3,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
     else:
